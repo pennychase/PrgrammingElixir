15793d96fae9dd04ef88d33917dc911910e1f4d5
Penny Chase <mpchase@mac.com>
2024-12-27 08:18:58 -0500

    center string in MyString

--- MyString.exs
+++ MyString.exs
@@ -1,9 +1,25 @@
 defmodule MyString do
 
+    # character lists
     def printable_ascii?(str), do: Enum.all?(str, fn ch -> ch in ?\s .. ?~ end)
 
     def anagram?(word1, word2), do: Enum.sort(word1) == Enum.sort(word2)
 
     def palindrome?(word), do: Enum.reverse(word) == word
+
+    # binary strings
+    def center(strs) do
+        {strs_lens, max_len} = _make_inputs(strs)
+        strs_lens |> Enum.map(&IO.puts(_center_one(&1, max_len)))
+    end
+
+    defp _center_one({str, len}, max_len), do: String.pad_leading(str, len + div(max_len - len, 2))
+
+    defp _make_inputs(strs) do
+        Enum.map_reduce(strs, 0, fn str, acc ->
+            len = String.length(str)
+            {{str, len}, max(len, acc)} end)
+    end
+
 
 end
