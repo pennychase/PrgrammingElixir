09e005b3add280a673107347845d2222cc2d3781
Penny Chase <mpchase@mac.com>
2024-12-28 18:53:50 -0500

    Github Issues

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -9,7 +9,9 @@ defmodule Issues.CLI do
   """
   
   def run(argv) do
-    parse_args(argv)
+    argv
+      |> parse_args
+      |> process
   end
 
   @doc """
@@ -21,13 +23,56 @@ defmodule Issues.CLI do
   Return a tuple of `{user, project, count}` or :help
   """
   def parse_args(argv) do
-    parse = OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
-    case parse do
-      {[help: true], _, _} -> :help
-      {_, [user, project, count], _} -> {user, project, count}
-      {_, [user, project], _} -> {user, project, @default_count}
-      _ -> :help
-    end
+    OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
+      |> elem(1)
+      |> args_to_internal_representation()
+  end
+
+  def args_to_internal_representation([user, project, count]) do
+    { user, project, count }
+  end
+
+  def args_to_internal_representation([user, project]) do
+    { user, project, @default_count }
+  end
+
+  def args_to_internal_representation(_) do # help (-h or --help) or bad argument
+    :help
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [count | #{@default_count}]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, count}) do
+    Issues.GithubIssues.fetch(user, project)
+      |> decode_response
+      |> sort_into_descending_order()
+      |> last(count)
+      |> Issues.TableFormatter.print_table_for_columns(["number", "created_at", "title"])
+  end
+
+  def decode_response({:ok, body}), do: body
+
+  def decode_response({:error, error}) do
+    IO.puts "Error fetching from GitHub: #{error["message"]}"
+    System.halt(2)
+  end
+
+  def sort_into_descending_order(list_of_issues) do
+    list_of_issues
+      |> Enum.sort(fn i1, i2 ->
+            i1["created_at"] >= i2["created_at"]
+          end)
+  end
+
+  def last(list, count) do
+    list
+      |> Enum.take(count)
+      |> Enum.reverse
   end
 
 end
