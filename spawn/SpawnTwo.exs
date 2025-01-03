7aa9027848097ba9a1af3a7b5b22ea9c388b42b3
Penny Chase <mpchase@mac.com>
2025-01-03 18:15:58 -0500

    Modifed SpawnTwo to have single function for clients. Ticker example (low level process registration)

--- spawn/SpawnTwo.exs
+++ spawn/SpawnTwo.exs
@@ -1,35 +1,66 @@
 defmodule SpawnTwo do
 
-  def alice do
-    receive do
-      {sender, "alice"} -> send sender, {self(), "alice"}
-    end
-  end
+#   def alice do
+#     receive do
+#       {sender, "alice"} -> send sender, {self(), "alice"}
+#     end
+#   end
 
-  def bob do
-    receive do
-      {sender, "bob"} -> send sender, {self(), "bob"}
-    end
+#   def bob do
+#     receive do
+#       {sender, "bob"} -> send sender, {self(), "bob"}
+#     end
+#   end
+
+#   def run do
+    
+#     alice = spawn(SpawnTwo, :alice,[])
+#     bob = spawn(SpawnTwo, :bob,[])
+
+#     send alice, {self(), "alice"}
+#     send bob, {self(), "bob"}
+
+#     receive do 
+#         {_sender, "alice"} -> IO.puts "alice replied first"
+#         {_sender, "bob"} -> IO.puts "bob replied first"
+#     end 
+
+#       receive do 
+#         {_sender, "alice"} -> IO.puts "alice replied second"
+#         {_sender, "bob"} -> IO.puts "bob replied second"
+#     end 
+
+#   end
+
+# end
+
+  def respond do
+      receive do
+        {sender, msg} ->
+          send sender, { :ok, "#{msg}" } 
+          respond()
+      end
   end
 
   def run do
     
-    alice = spawn(SpawnTwo, :alice,[])
-    bob = spawn(SpawnTwo, :bob,[])
+    alice = spawn(SpawnTwo, :respond, [])  
+    bob = spawn(SpawnTwo, :respond, [])
 
-    send alice, {self(), "alice"}
+    send alice, { self(), "alice" }
     send bob, {self(), "bob"}
 
     receive do 
-        {_sender, "alice"} -> IO.puts "alice replied first"
-        {_sender, "bob"} -> IO.puts "bob replied first"
+      {_sender, "alice"} -> IO.puts "alice replied first"
+      {_sender, "bob"} -> IO.puts "bob replied first"
     end 
 
-      receive do 
-        {_sender, "alice"} -> IO.puts "alice replied second"
-        {_sender, "bob"} -> IO.puts "bob replied second"
+    receive do 
+      {_sender, "alice"} -> IO.puts "alice replied second"
+      {_sender, "bob"} -> IO.puts "bob replied second"
     end 
 
   end
+
 
 end
